02196f5a62be8663f24d0b4b6b64a70bc5e390cd
Andy <andy@gaoding.com>
2020-06-29 20:03:01 +0800

    feat: 添加libyuv.a文件

---
+++ examples/Android/XRender/corerender/includes/libyuv/arm64-v8a/include/libyuv/version.h
@@ -0,0 +1,17 @@
+/*
+ *  Copyright 2012 The LibYuv Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS. All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef INCLUDE_LIBYUV_VERSION_H_
+#define INCLUDE_LIBYUV_VERSION_H_
+
+#define LIBYUV_VERSION 1660
+#define MT_LIBYUV_VERSION  "1.1.0.0" //"合并一次master分支则+1 . 有较大变动（新增多个功能且有修改bug或者优化代码）则+1 . 新增功能则+1 . 修改bug则+1"
+
+#endif  // INCLUDE_LIBYUV_VERSION_H_
