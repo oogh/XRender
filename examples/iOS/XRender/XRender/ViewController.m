6a2088cb64a78b5e46fd450fce9ac210bb384328
oogh <oogh216@163.com>
2020-07-05 13:47:47 +0800

    feat: 更新iOS平台Demo页面

--- examples/iOS/XRender/XRender/ViewController.m
+++ examples/iOS/XRender/XRender/ViewController.m
@@ -12,6 +12,8 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet XView *displayView;
 @property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UITextField *currentTextField;
+@property (weak, nonatomic) IBOutlet UITextField *durationTextField;
 
 @end
 
@@ -20,26 +22,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.slider.value = 0;
+    self.currentTextField.placeholder = @"0";
+    self.durationTextField.placeholder = @"0";
     __weak typeof(self) weakSelf = self;
     self.displayView.progressChangeCallback = ^(long current, long duration) {
         __strong typeof(weakSelf) strongSelf = weakSelf;
         dispatch_async(dispatch_get_main_queue(), ^{
-            strongSelf.slider.minimumValue = 0;
             strongSelf.slider.maximumValue = duration;
             strongSelf.slider.value = current;
+            
+            strongSelf.currentTextField.text = [NSString stringWithFormat: @"%ld", current];
+            strongSelf.durationTextField.text = [NSString stringWithFormat: @"%ld", duration];;
         });
     };
 }
 
-- (IBAction)onOncePressClick:(id)sender {
-    NSString *filename = [[NSBundle mainBundle] pathForResource:@"douyin_700x1240" ofType:@"mp4"];
-    [self.displayView setInput:filename];
-    [self.displayView start];
-    
+- (IBAction)onOncePressClick:(UIButton *)sender {
+        NSString *filename = [[NSBundle mainBundle] pathForResource:@"douyin_700x1240" ofType:@"mp4"];
+        [self.displayView setInput:filename];
+        [self.displayView start];
 }
-- (IBAction)onAddClick:(UIButton *)sender {
+
+- (IBAction)onSetInputClick:(UIButton *)sender {
     NSString *filename = [[NSBundle mainBundle] pathForResource:@"douyin_700x1240" ofType:@"mp4"];
-    [self.displayView setInput:filename];
+        [self.displayView setInput:filename];
+}
+
+
+- (IBAction)onPrepareClick:(UIButton *)sender {
+    
 }
 
 - (IBAction)onStartClick:(UIButton *)sender {
@@ -57,11 +68,11 @@
 - (IBAction)onProgressBegin:(UISlider *)sender {
 }
 
-- (IBAction)onProgressEnd:(UISlider *)sender {
-}
-
 - (IBAction)onProgressChanged:(UISlider *)sender {
     [self.displayView seekTo:(long)sender.value];
+}
+
+- (IBAction)onProgressEnd:(UISlider *)sender {
 }
 
 @end
