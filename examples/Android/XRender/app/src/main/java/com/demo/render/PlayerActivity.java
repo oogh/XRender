bd39ee3af772b2d451f21a63a992d5aa6e449cc6
Andy <andy@gaoding.com>
2020-08-03 10:21:22 +0800

    feat: 修复Demo测试使用素材

--- examples/Android/XRender/app/src/main/java/com/demo/render/PlayerActivity.java
+++ examples/Android/XRender/app/src/main/java/com/demo/render/PlayerActivity.java
@@ -28,7 +28,7 @@ public class PlayerActivity extends AppCompatActivity {
         findViewById(R.id.btn_once_press).setOnClickListener(v -> {
             // "/sdcard/Android/data/com.demo.render/files/jieqian_720x1280.yuv"
             String path = getPrivateExternalFilesDir(this, "");
-            mDisplayView.setInput(path + "hechengkazhen.mp4");
+            mDisplayView.setInput(path + "720.mp4");
             mDisplayView.start();
         });
 
