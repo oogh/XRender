00c5712911a5d0c0259b6e6b85ada4549c030ca0
oogh <oogh216@163.com>
2020-09-15 17:41:34 +0800

    feat: 设置PacketQueue的最大容量为250
    由于读包在readWorkThread中，存在音频包全部读完，视频才开始读取的问题，而PacketQueue是有wait/notify的，因此，可能音频很快占满PacketQueue导致读包线程wait，无法读取到视频包。
    
    这里简单调大PacketQueue的容量，后续可以将音/视频流读包放在不同线程中去处理，或者移除PacketQueue中的wait/notify

--- corerender/XPacketQueue.h
+++ corerender/XPacketQueue.h
@@ -57,7 +57,7 @@ public:
     void flush();
     
 private:
-    static const size_t PQ_DEFAULT_CAPACITY = 25;
+    static const size_t PQ_DEFAULT_CAPACITY = 250;
     
 private:
     std::queue<std::shared_ptr<Packet>> mPacketQueue;
