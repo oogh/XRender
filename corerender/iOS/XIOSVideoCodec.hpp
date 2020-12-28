5991ca62c9bf1097cc7e2f7b2579571b275312fb
oogh <oogh216@163.com>
2020-12-28 11:27:57 +0800

    feat: 临时提交

--- corerender/iOS/XIOSVideoCodec.hpp
+++ corerender/iOS/XIOSVideoCodec.hpp
@@ -9,9 +9,32 @@
 #ifndef XIOSVideoCodec_hpp
 #define XIOSVideoCodec_hpp
 
+#import <AVFoundation/AVFoundation.h>
+
+#include <memory>
+#include <string>
+
+struct XImage;
+
 class XIOSVideoCodec {
 public:
     XIOSVideoCodec();
+    
+    ~XIOSVideoCodec();
+    
+    void setFilename(const std::string& filename);
+    
+    std::shared_ptr<XImage> getImage(long clock);
+    
+private:
+    int open();
+    
+    void close();
+    
+private:
+    std::string mFilename;
+    AVAssetReader* mReader;
+    AVAssetReaderTrackOutput* mVideoReaderOutput;
 };
 
 #endif /* XIOSVideoCodec_hpp */
--- corerender/iOS/XIOSVideoCodec.mm
+++ corerender/iOS/XIOSVideoCodec.mm
@@ -7,7 +7,66 @@
 //
 
 #include "XIOSVideoCodec.hpp"
+#include "XLogger.hpp"
+#include "XImage.hpp"
 
 XIOSVideoCodec::XIOSVideoCodec() {
+    
+}
+
+XIOSVideoCodec::~XIOSVideoCodec() {
+    
+}
+
+void XIOSVideoCodec::setFilename(const std::string& filename) {
+    mFilename = filename;
+}
+
+std::shared_ptr<XImage> XIOSVideoCodec::getImage(long clock) {
+    CMSampleBufferRef buffer = [mVideoReaderOutput copyNextSampleBuffer];
+    auto image = std::make_shared<XImage>();
+    image->pixels[3] = reinterpret_cast<uint8_t*>(buffer);
+    
+    CMTime pts = CMSampleBufferGetPresentationTimeStamp(buffer);
+    image->pts = static_cast<long>(CMTimeGetSeconds(pts) * 1000);
+    
+    CMTime duration = CMSampleBufferGetDuration(buffer);
+    image->duration = static_cast<long>(CMTimeGetSeconds(duration) * 1000);
+    
+//    CMVideoFormatDescription desc = CMSampleBufferGetFormatFormatDescription(buffer);
+    return nullptr;
+}
+
+int XIOSVideoCodec::open() {
+
+    NSString* path = [NSString stringWithCString:mFilename.data()
+                                        encoding:[NSString defaultCStringEncoding]];
+    AVAsset* asset = [AVAsset assetWithURL:[NSURL fileURLWithPath:path]];
+    
+    NSError* err;
+    mReader = [AVAssetReader assetReaderWithAsset:asset error:&err];
+    if (err) {
+        LOGE("[XIOSVideoCodec] assetReaderWithAsset failed: %@", err.localizedDescription);
+        return -1;
+    }
+    AVAssetTrack* videoTrack = [[mReader.asset tracksWithMediaType:AVMediaTypeVideo] firstObject];
+    
+    NSDictionary* readSettings = @{
+        (id)kCVPixelBufferPixelFormatTypeKey: @(kCVPixelFormatType_32BGRA),
+        (id)kCVPixelBufferIOSurfacePropertiesKey: [NSDictionary dictionary]
+    };
+    mVideoReaderOutput = [AVAssetReaderTrackOutput assetReaderTrackOutputWithTrack:videoTrack
+                                                                    outputSettings:readSettings];
+    
+    if ([mReader canAddOutput:mVideoReaderOutput]) {
+        [mReader addOutput:mVideoReaderOutput];
+    }
+    
+    [mReader startReading];
+    
+    return 0;
+}
+
+void XIOSVideoCodec::close() {
     
 }
